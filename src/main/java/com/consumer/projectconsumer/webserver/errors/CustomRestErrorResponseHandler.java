146aaaa3db74f7ccee9ab49bded08eb5be598b91
mariaost253 <mariaost253@gmail.com>
2020-02-10 15:25:23 +0200

    commit

--- src/main/java/com/consumer/projectconsumer/webserver/errors/CustomRestErrorResponseHandler.java
+++ src/main/java/com/consumer/projectconsumer/webserver/errors/CustomRestErrorResponseHandler.java
@@ -1,5 +1,4 @@
 package com.consumer.projectconsumer.webserver.errors;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,12 +7,6 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 public class CustomRestErrorResponseHandler extends ResponseEntityExceptionHandler {
 
-
-    /**
-     * Gets HttpClientErrorException.BadRequest and returns ResponseEntity object with CustomError
-     * @param badRequestError
-     * @return
-     */
     public static ResponseEntity<?> handleBadRequest(HttpClientErrorException.BadRequest badRequestError) {
         String error = "Bad request";
         final CustomError ce = new CustomError(badRequestError.getStatusCode(), badRequestError.getMessage(), error);
@@ -21,11 +14,7 @@ public class CustomRestErrorResponseHandler extends ResponseEntityExceptionHandl
         return new ResponseEntity<Object>(ce, new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 
-    /**
-     * Gets ID that is not found and returns ResponseEntity object with 404 status code
-     * @param id
-     * @return
-     */
+
     public static ResponseEntity<?> handleNotFoundException(Integer id) {
         String message = "'" + id + "' not found!";
         final CustomError ce = new CustomError(HttpStatus.NOT_FOUND, message, message);
